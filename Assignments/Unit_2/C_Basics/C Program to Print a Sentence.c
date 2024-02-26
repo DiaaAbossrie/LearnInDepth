ae393cc70662879fee0c4578506d7933f3ade12b
Diaa <superdodo.2015@gmail.com>
2024-02-27 00:06:28 +0200

    First example well formatted

--- Assignments/Unit_2/C_Basics/C Program to Print a Sentence.c
+++ Assignments/Unit_2/C_Basics/C Program to Print a Sentence.c
@@ -1,5 +1,14 @@
-#include <stdio.h>>
+/* Author: Diaa Abossrie
+ *
+ * Title: Simple C Program
+ *
+ * Description: This is a simple C program that prints "C Programming".
+ */
 
-void main(void){
-	printf("C Programming");
+#include <stdio.h>
+
+int main(void) {
+    printf("C Programming\n"); // Print the message to the console
+    return 0; // Return 0 to indicate successful execution
 }
+
